7d48e88040dc3ca5f340df2fde2047745836aa4e
taslima haque <tahiadu@gmail.com>
2020-02-08 23:45:30 -0600

    Add files via upload

--- Exploratory_Analysis_PHTrans.R
+++ Exploratory_Analysis_PHTrans.R
@@ -1,4 +1,4 @@
-setwd("/home/taslima/data/JuengerLab/ALL_RNASeq/Bhashkar_RNASeq/V4")
+setwd("/home/taslima/data/JuengerLab/ALL_RNASeq/Bhashkar_RNASeq/V5")
 
 rm(list=ls())
 
@@ -9,30 +9,14 @@ library("BiocParallel")
 library("vsn")
 library(edgeR)
 library(xlsx)
+library(tidyverse)
+library(PCA)
 
 register(MulticoreParam(workers=8))
 
-Ortho<-read.csv("tableS3_orthologDatabase.csv",check.names = F)
-dim(Ortho)
-Ortho<-na.omit(Ortho)
-dim(Ortho)
-Ortho$ID<-sapply(1:dim(Ortho)[1], function(x) paste( c("Ortho",sprintf("%06d", x)),collapse = "_"))
+Design<-read.csv("PHTrans_Final_Meta.csv",row.names = 6)
+dat<-read.table("../V5/PHTrans_Count_HALREF.tab",header = T,stringsAsFactors = F,check.names = F)
 
-datH<-read.csv("Counts_HAL_rename.csv",check.names = F,row.names =1)
-colnames(datH)<-gsub( "_-[0-9][0-9]|--[0-9][0-9]|_-[0-9]","",colnames(datH))
-rownames(datH)<-gsub(".v2.1","",rownames(datH)[1:nrow(datH)])
-datH<-datH[Ortho$HAL2,]
-
-Design<-read.csv("Metasheet_lib.csv",row.names = 7,check.names = F,na.strings = "NA")
-Design<-Design[-which(is.na(Design$location)),]
-Design<-Design[-which(Design$SITE=="BFL_2"),]
-Design<-Design[-which(rownames(Design) %in% c("PD19-30","PD19-5")),]
-
-ID<-intersect(rownames(Design), colnames(datH))
-
-dat<-datH[,ID]
-
-Design<-Design[which(rownames(Design) %in% ID),]
 dat<-dat[,rownames(Design)]
 dat<-dat[-which(as.vector(rowMeans(dat,na.rm = F)) <1),]
 
@@ -51,24 +35,16 @@ head(tdat)
 dim(tdat)
 tdat$lib_ID<-rownames(tdat)
 
-PheDes<-read.csv("GB_Metalib_Pheno.csv",check.names = F)
-PheDes$lib_ID<-as.character(PheDes$lib_ID)
-PheDes<-PheDes[-which(PheDes$location == "Mexico"),] #Remove Mexican samples
-HAL2remove<-setdiff(unique(PheDes$genotype[which(grepl("HAL",PheDes$genotype))]),c("HAL2"))
-FIL2remove<-setdiff(unique(PheDes$genotype[which(grepl("FIL",PheDes$genotype))]),c("FIL2"))
-PheDes<-PheDes[-which(PheDes$genotype %in% c("GIL42A",HAL2remove,FIL2remove)),] # Remove outlier
-PheDes$location[which(PheDes$location =="Coastal")] <-"Filipes"
-PheDes$Population<-ifelse( PheDes$location %in% c("CenTex","West"),"HAL_I",ifelse(PheDes$location %in% c("Sym","TexMex"),"HAL_II","FIL"))
-PheDes$Population<-factor(PheDes$Population,levels = c("FIL","HAL_I","HAL_II"))
+PheDes<-Design[,-c(3)]
+PheDes$lib_ID<-rownames(PheDes)
 PheExp<-merge(PheDes,tdat,by="lib_ID",all.x=F, all.y=F)
-PheExp<-PheExp[,-c(1:2,4,7:15)]
 
-ExpCount <- as.tibble(PheExp[,c(1:5)])  %>% group_by(genotype,location,SITE,Group,Population) %>% summarise(n=n())
+ExpCount <- as.tibble(PheExp) %>% select(genotype,location,SITE,Group,Population)  %>% group_by(genotype,location,SITE,Group,Population) %>% summarise(n=n())
 
 GenCorPheExp <- as.tibble(PheExp[-which(ExpCount$n < 3),])  %>% group_by(genotype,location,SITE,Group,Population) %>% summarise_all(funs(mean))
 rownames(GenCorPheExp)<-paste(GenCorPheExp$genotype,GenCorPheExp$SITE,sep = "_")
 dim(GenCorPheExp)
-tGenCorPheExp<-as.data.frame(t(GenCorPheExp[,-c(1:5)]))
+tGenCorPheExp<-as.data.frame(t(GenCorPheExp[,-c(1:19)]))
 colnames(tGenCorPheExp)<-rownames(GenCorPheExp)
 
 #Get the variances of each gene to select top 1000 genes for PCA
@@ -78,28 +54,196 @@ colnames(var)<-"GeneVariance"
 TopVarGene<-rownames(var)[order(var$GeneVariance,decreasing = T)][1:1000]
 dat_sel<-tGenCorPheExp[which(rownames(tGenCorPheExp) %in% TopVarGene  ),]
 colnames(dat_sel)<-paste(GenCorPheExp$genotype,GenCorPheExp$SITE,sep = "_")
-dat<-cbind(GenCorPheExp[,c(5,3)],na.omit(t(dat_sel)))
+Expdat<-cbind(GenCorPheExp[,c(5,3)],na.omit(t(dat_sel)))
 
-p1<-PlotPCA(dat,axis1 = 1,axis2 = 2,shape = 2,col.ind=1,ncp = 5)
-p2<-PlotPCA(dat,axis1 = 2,axis2 = 3,shape = 2,col.ind=1,ncp = 5)
-p3<-PlotPCA(dat,axis1 = 3,axis2 = 4,shape = 2,col.ind=1,ncp = 5)
-p4<-PlotPCA(dat,axis1 = 4,axis2 = 5,shape = 2,col.ind=1,ncp = 5)
+####### CODE FOR PCA
+### It is raw right now
+## Soon will update as a package
 
-grid.arrange(p1,p2,p3,p4, ncol=2, nrow = 2, top= "")
+### Axis 1 and 2
+axis1=1
+axis2=2
+shape=2
+col.ind=1
+axis<-c(axis1,axis2)
+
+seldat<-Expdat[,-c(shape,col.ind)]
+res.pca <- PCA(seldat, graph = FALSE,ncp = 5)
+perVar <- res.pca$eig[,2]
+ind <- data.frame(res.pca$ind$coord[, axis, drop = FALSE])
+var<-as.data.frame(res.pca$var$coord)
+
+ind$shape<-as.vector(Expdat[,shape])
+ind$col<-as.vector(Expdat[,col.ind])
+xlab=paste("Axis",axis1," (",round(as.numeric(perVar[axis1]),2),"%)",sep = "")
+ylab=paste("Axis",axis2," (",round(as.numeric(perVar[axis2]),2),"%)",sep = "")
+
+floorOrceiling<-function(x) {
+  if (x>= 0) {y<-ceiling(x); return(y)} else {
+    y<-floor(x); return(y)
+  }
+}
+
+MinMax<-function (ind) {
+  lims<-c()
+  for (i in 1:2) {
+    if ( abs(min(ind[,i])) >= abs(max(ind[,i])) ) 
+    { 
+      vmax<-abs(floorOrceiling(min(ind[,i])) )
+      vmin<-floorOrceiling(min(ind[,i])) 
+    } else {
+      vmax<-abs(floorOrceiling(max(ind[,i])) )
+      vmin<-(floorOrceiling(max(ind[,i]))*-1)
+    }
+    lims<-append(lims,c(vmin,vmax))
+  }
+  return(lims) 
+}
+
+lims<-MinMax(ind)
+
+## Need to work on shape a bit
+shapelist<-c(16,17,18,19,20)
+
+## And color too
+collist<- c("#000000", "#E69F00", "#009E73", "#0072B2", "#D55E00", "#CC79A7")
+#collist<- c( "#E69F00", "#009E73", "#0072B2", "#D55E00", "#CC79A7")
+#collist<- c( "#E69F00", "#009E73",  "#CC79A7")
+######### Plot individulas
+title<-paste("PCA on Axis ",axis1, " and ",axis2,sep = "")
+
+p1<-ggplot(ind)+geom_point(aes(x=ind[,1],y=ind[,2],shape=ind$shape,colour=ind$col),size=3, alpha=0.75)+
+    geom_hline(yintercept = 0,linetype=2)+geom_vline(xintercept = 0,linetype=2)+
+    theme_bw() + labs(x =xlab, y=ylab,title = title,shape=as.name(colnames(Expdat)[shape]),colour=as.name(colnames(Expdat)[col.ind]))+ 
+    scale_shape_manual(values=shapelist)+
+    scale_color_manual(values=collist)+
+    scale_x_continuous(limits = lims[1:2])+
+    scale_y_continuous(limits = lims[3:4])+
+    theme(plot.title = element_text(hjust = 0.5,size=14, face = "bold"),
+          legend.title = element_text(size=10, face="bold"),
+          legend.text = element_text(size=10),
+          legend.position=c(1.05, 0.75),
+          axis.title = element_text(size=12,face="bold"),
+          panel.border = element_blank(), 
+          panel.grid.major = element_blank(),
+          #panel.grid.minor = element_blank(), 
+          axis.line = element_line(colour = "black"),
+          axis.text=element_text(size=12,vjust=1),
+          plot.margin = margin(0.25, 5, 0.25, 0.25, "cm")
+          #axis.ticks.x = element_blank()
+    )
+
+############## Axis 2 and 3
+
+axis1=2
+axis2=3
+shape=2
+col.ind=1
+axis<-c(axis1,axis2)
+
+seldat<-Expdat[,-c(shape,col.ind)]
+res.pca <- PCA(seldat, graph = FALSE,ncp = 5)
+perVar <- res.pca$eig[,2]
+ind <- data.frame(res.pca$ind$coord[, axis, drop = FALSE])
+var<-as.data.frame(res.pca$var$coord)
+
+ind$shape<-as.vector(Expdat[,shape])
+ind$col<-as.vector(Expdat[,col.ind])
+xlab=paste("Axis",axis1," (",round(as.numeric(perVar[axis1]),2),"%)",sep = "")
+ylab=paste("Axis",axis2," (",round(as.numeric(perVar[axis2]),2),"%)",sep = "")
+
+floorOrceiling<-function(x) {
+  if (x>= 0) {y<-ceiling(x); return(y)} else {
+    y<-floor(x); return(y)
+  }
+}
+
+MinMax<-function (ind) {
+  lims<-c()
+  for (i in 1:2) {
+    if ( abs(min(ind[,i])) >= abs(max(ind[,i])) ) 
+    { 
+      vmax<-abs(floorOrceiling(min(ind[,i])) )
+      vmin<-floorOrceiling(min(ind[,i])) 
+    } else {
+      vmax<-abs(floorOrceiling(max(ind[,i])) )
+      vmin<-(floorOrceiling(max(ind[,i]))*-1)
+    }
+    lims<-append(lims,c(vmin,vmax))
+  }
+  return(lims) 
+}
+
+lims<-MinMax(ind)
+
+## Need to work on shape a bit
+shapelist<-c(16,17,18,19,20)
+
+## And color too
+collist<- c("#000000", "#E69F00", "#009E73", "#0072B2", "#D55E00", "#CC79A7")
+#collist<- c( "#E69F00", "#009E73", "#0072B2", "#D55E00", "#CC79A7")
+#collist<- c( "#E69F00", "#009E73",  "#CC79A7")
+######### Plot individulas
+title<-paste("PCA on Axis ",axis1, " and ",axis2,sep = "")
+
+p2<-ggplot(ind)+geom_point(aes(x=ind[,1],y=ind[,2],shape=ind$shape,colour=ind$col),size=3, alpha=0.75)+
+  geom_hline(yintercept = 0,linetype=2)+geom_vline(xintercept = 0,linetype=2)+
+  theme_bw() + labs(x =xlab, y=ylab,title = title,shape=as.name(colnames(Expdat)[shape]),colour=as.name(colnames(Expdat)[col.ind]))+ 
+  scale_shape_manual(values=shapelist)+
+  scale_color_manual(values=collist)+
+  scale_x_continuous(limits = lims[1:2])+
+  scale_y_continuous(limits = lims[3:4])+
+  theme(plot.title = element_text(hjust = 0.5,size=14, face = "bold"),
+        legend.title = element_text(size=10, face="bold"),
+        legend.text = element_text(size=10),
+        legend.position=c(1.05, 0.75),
+        axis.title = element_text(size=12,face="bold"),
+        panel.border = element_blank(), 
+        panel.grid.major = element_blank(),
+        #panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.text=element_text(size=12,vjust=1),
+        plot.margin = margin(0.25, 5, 0.25, 0.25, "cm")
+        #axis.ticks.x = element_blank()
+  )
+
+
+
+grid.arrange(p1,p2, ncol=2, nrow = 1, top= "")
+
+########### COV
+head(PheExp)[c(1:5),c(1:20)]
+PheExp2<-PheExp[,-c(1,2,4:13)]
+
+CV<-function(x){ 
+  cv<-round( 100*((sd(x,na.rm = T))/mean(x,na.rm=T)), 2)
+  return(cv)
+}
+datCV<-PheExp2 %>% group_by(Group) %>% summarise_at(colnames(PheExp2)[-(1:3)], CV)
+df<-datCV %>% gather(GeneName, CV, colnames(datCV)[-1])
+df$SITE<-rep("BFL",dim(df)[1])
+
+datCVSite<-PheExp2 %>% group_by(SITE) %>% summarise_at(colnames(PheExp2)[-(1:3)], CV)
+dfSite<-datCVSite %>% gather(GeneName, CV, colnames(datCV)[-1])
+
+ggplot() + 
+  geom_line( data=df, mapping=aes(x=CV,color=Group,linetype=SITE),stat="density",size=0.5,alpha=1,show.legend  = T)+
+  geom_line( data = dfSite,mapping = aes(x=CV,linetype=SITE),stat="density",size=0.8,alpha=0.75,show.legend = F)+
+  theme_classic(base_size = 14)+labs(x="Coefficient of Variantion (CV)")+
+  scale_color_manual(values = c("#E69F00", "#56B4E9", "#009E73","#F0E442", "#0072B2", "#D55E00", "#CC79A7"),
+                     #c("#999999", "#E69F00", "#56B4E9", "#009E73","#F0E442", "#0072B2", "#D55E00", "#CC79A7"),
+                     #c("#E69F00", "#56B4E9", "#009E73","#F0E442", "#0072B2", "#D55E00", "#CC79A7"),
+                     #c("#000000", "#E69F00", "#009E73", "#0072B2", "#D55E00", "#CC79A7"),
+                     name="GroupXSITE",
+                     breaks=levels(factor(df$Group)),
+                     labels= levels(factor(df$Group)))
+
 
 ########## Pheno PCA
 
 ############ genetic correlation of pheno
-PheDes<-read.csv("GB_Metalib_Pheno.csv",check.names = F)
-PheDes$lib_ID<-as.character(PheDes$lib_ID)
-PheDes<-PheDes[-which(PheDes$location == "Mexico"),] #Remove Mexican samples
-HAL2remove<-setdiff(unique(PheDes$genotype[which(grepl("HAL",PheDes$genotype))]),c("HAL2"))
-FIL2remove<-setdiff(unique(PheDes$genotype[which(grepl("FIL",PheDes$genotype))]),c("FIL2"))
-PheDes<-PheDes[-which(PheDes$genotype %in% c("GIL42A",HAL2remove,FIL2remove)),] # Remove outlier
-PheDes$location[which(PheDes$location =="Coastal")] <-"Filipes"
-PheDes$Population<-ifelse( PheDes$location %in% c("CenTex","West"),"HAL_I",ifelse(PheDes$location %in% c("Sym","TexMex"),"HAL_II","FIL"))
-PheDes$Population<-factor(PheDes$Population,levels = c("FIL","HAL_I","HAL_II"))
-PheDes<-PheDes[,c(2,17,4,7:15)]
+head(PheDes)
+PheDes<-PheDes[,c(1,2,14,4:12)]
 
 #colnames(PheMat)<-c("PLOT_GL", "BIOMASS",  "TC",  "AREA","SLA", "FRESH", "DRY","RWC",  "OP", "MDWP")
 
@@ -150,9 +294,10 @@ PCAPhe %>%
 ### MDWP and OP totally bimodal if we consider total population; this bimodal pattern is for SITE
 ### sd of RWC of KINGS are higher that BFL therefore have very different disribution and not has a clear bimodal separartion
 
-PCAPhe<-na.omit(PCAPhe)
 
 ####### RUN PCA on genetically identical pooled samples
+####### Will change this PCA later
+###### SITE should be in different color
 library(ade4)
 library(factoextra)
 library(magrittr)
@@ -182,49 +327,4 @@ fviz_pca_biplot(res.pca, label ="var",axes = c(1,2),
                 repel = TRUE,
                 addEllipses=F, ellipse.level=0.75)+labs(title="PCA on Axis 1 and 2")+
   theme(plot.title = element_text(hjust = 0.5))  
-
-########### COV
-
-PheDes<-read.csv("GB_Metalib_Pheno.csv",check.names = F)
-PheDes$lib_ID<-as.character(PheDes$lib_ID)
-PheDes<-PheDes[-which(PheDes$location == "Mexico"),] #Remove Mexican samples
-PheDes<-PheDes[-which(PheDes$genotype %in% c("FIL216","GIL42A")),] # Remove outlier
-PheDes$location[which(PheDes$location =="Coastal")] <-"Filipes"
-PheDes$Population<-ifelse( PheDes$location %in% c("CenTex","West"),"HAL_I",ifelse(PheDes$location %in% c("Sym","TexMex"),"HAL_II","FIL"))
-PheDes$Population<-factor(PheDes$Population,levels = c("FIL","HAL_I","HAL_II"))
-PheDes$Group<-paste(PheDes$Population,PheDes$SITE,sep = "_")
-PheExp<-merge(PheDes,tdat,by="lib_ID",all.x=F, all.y=F)
-PheExp<-PheExp[,-c(1:2,4,6:15)]
-
-ExpCount <- as.tibble(PheExp[,c(1:4)])  %>% group_by(genotype,SITE,Group) %>% summarise(n=n())
-
-ExpCount<-ExpCount[-which(ExpCount$n < 3),] 
-ExpCount$GenoSite<-paste(ExpCount$genotype,ExpCount$SITE,sep = "_")
-
-PheDes$GenoSite<-paste(PheDes$genotype,PheDes$SITE,sep="_")
-PheDes<-PheDes[which(PheDes$GenoSite %in% ExpCount$GenoSite ),]
-rownames(PheDes)<-PheDes$lib_ID
-
-CV<-function(x){ 
-  cv<-round( 100*((sd(x,na.rm = T))/mean(x,na.rm=T)), 2)
-  return(cv)
-}
-datCV<-PheExp %>% group_by(Group) %>% summarise_at(colnames(PheExp)[-(1:4)], CV)
-df<-datCV %>% gather(GeneName, CV, colnames(datCV)[-1])
-df$SITE<-rep("BFL",dim(df)[1])
-
-datCVSite<-PheExp %>% group_by(SITE) %>% summarise_at(colnames(PheExp)[-(1:4)], CV)
-dfSite<-datCVSite %>% gather(GeneName, CV, colnames(datCV)[-1])
-
-ggplot() + 
-  geom_line( data=df, mapping=aes(x=CV,color=Group,linetype=SITE),stat="density",size=0.5,alpha=1,show.legend  = T)+
-  geom_line( data = dfSite,mapping = aes(x=CV,linetype=SITE),stat="density",size=0.8,alpha=0.75,show.legend = F)+
-  theme_classic(base_size = 14)+labs(x="Coefficient of Variantion (CV)")+
-  scale_color_manual(values = c("#E69F00", "#56B4E9", "#009E73","#F0E442", "#0072B2", "#D55E00", "#CC79A7"),
-                     #c("#999999", "#E69F00", "#56B4E9", "#009E73","#F0E442", "#0072B2", "#D55E00", "#CC79A7"),
-                     #c("#E69F00", "#56B4E9", "#009E73","#F0E442", "#0072B2", "#D55E00", "#CC79A7"),
-                     #c("#000000", "#E69F00", "#009E73", "#0072B2", "#D55E00", "#CC79A7"),
-                     name="GroupXSITE",
-                     breaks=levels(factor(df$Group)),
-                     labels= levels(factor(df$Group)))
 
